31439d1c00d838157aa1f5b5ef70ad046ac4769f
Jacob Ashdown <jcbashdown@gmail.com>
2024-05-10 09:43:50 +0300

    Use OSM

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useRef } from 'react'
-import { MapContainer, ImageOverlay, SVGOverlay } from 'react-leaflet'
-import { CRS } from 'leaflet'
+import { MapContainer, ImageOverlay, SVGOverlay, TileLayer } from 'react-leaflet'
+import { CRS, LatLngBoundsExpression } from 'leaflet'
 
 import { useAppSelector } from '@/lib/redux/hooks'
 import { selectTrafficMap } from '@/lib/redux/store'
@@ -18,35 +18,27 @@ const Map = () => {
             mapRef.current.invalidateSize()
         }
     }, [])
+    const bounds: LatLngBoundsExpression = [
+        [-1.3993664696339427, 36.71360986600999], // Southwest corner
+        [-1.1848443097557453, 36.928186587201395], // Northeast corner
+    ]
 
     return (
         <MapContainer
             ref={mapRef}
-            center={[5000, 5000]}
-            minZoom={-2}
-            maxZoom={2}
-            zoom={-2}
-            crs={CRS.Simple}
+            center={[-1.279924177522545, 36.8187184497727]}
+            zoom={14}
+            minZoom={12}
             style={{ width: '100%', height: '100%' }}
-            maxBounds={[
-                [0, 0],
-                [10000, 10000],
-            ]}
+            maxBounds={bounds}
             maxBoundsViscosity={1}
         >
-            <ImageOverlay
-                url="/background-map.png"
-                bounds={[
-                    [0, 0],
-                    [10000, 10000],
-                ]}
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                opacity={0.5}
             />
-            <SVGOverlay
-                bounds={[
-                    [0, 0],
-                    [10000, 10000],
-                ]}
-            >
+            <SVGOverlay bounds={bounds}>
                 <svg viewBox="0 0 10000 10000">
                     <image href={`/traffic_svgs/${selectedTrafficMap}`} width="10000" height="10000" />
                 </svg>
